ca140b7948b4371e7e9c8d4f9b92a506d89dafbb
kihwn <cristine980@konkuk.ac.kr>
2025-01-29 03:41:36 +0900

    feat: email&code로 club 멤버 추가 controller 구현

--- backend/src/main/java/com/ssafy/dawata/domain/club/controller/ClubMemberController.java
+++ backend/src/main/java/com/ssafy/dawata/domain/club/controller/ClubMemberController.java
@@ -5,9 +5,13 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ssafy.dawata.domain.club.dto.request.JoinClubByCodeRequest;
+import com.ssafy.dawata.domain.club.dto.request.JoinClubByEmailRequest;
 import com.ssafy.dawata.domain.club.dto.response.ClubMemberInfoResponse;
 import com.ssafy.dawata.domain.club.service.ClubMemberService;
 
@@ -18,8 +22,35 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ClubMemberController {
 	private final ClubMemberService clubMemberService;
+
 	@GetMapping
-	public List<ClubMemberInfoResponse> getClubMembers(@PathVariable Long clubId){
-		return clubMemberService.getClubMembers(clubId);
+	public ResponseEntity<List<ClubMemberInfoResponse>> getClubMembers(@PathVariable Long clubId) {
+		List<ClubMemberInfoResponse> members = clubMemberService.getClubMembers(clubId);
+		return ResponseEntity.ok(members);
+	}
+
+	//이메일로 클럽멤버 추가
+	@PostMapping("/email")
+	public ResponseEntity<Boolean> addClubMemberByEmail(@PathVariable Long clubId,
+		@RequestBody JoinClubByEmailRequest request) {
+		try {
+			clubMemberService.addClubMemberByEmail(request, clubId);
+			return ResponseEntity.ok(true);
+		} catch (IllegalArgumentException e) {
+			return ResponseEntity.badRequest().body(false);
+		}
+	}
+
+	//코드로 클럽멤버 추가
+	@PostMapping("/code")
+	public ResponseEntity<Boolean> addClubMemberByCode(
+		@PathVariable Long clubId,
+		@RequestBody JoinClubByCodeRequest request) {
+		try {
+			clubMemberService.addClubMemberByCode(request, clubId);
+			return ResponseEntity.ok(true);
+		} catch (IllegalArgumentException e) {
+			return ResponseEntity.badRequest().body(false);
+		}
 	}
 }
