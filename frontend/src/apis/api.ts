93c54c30b4aaa2023e5c4cec1bcd940cb2f3d331
DESKTOP-4QCTMKG\SSAFY <jemilykoo@gmail.com>
2025-02-04 17:07:45 +0900

    style: prettierrc 변경경

--- frontend/src/apis/api.ts
+++ frontend/src/apis/api.ts
@@ -7,29 +7,29 @@ const api = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
-});
+})
 
 // 요청 인터셉터
 api.interceptors.request.use(
-    (config) => {
-      console.log('📤 요청 보냄:', config.url);
-      return config;
-    },
-    (error) => {
-      console.error('🚨 요청 오류:', error);
-      return Promise.reject(error);
-    }
-  );
-  
-  api.interceptors.response.use(
-    (response) => {
-      console.log('✅ 응답 성공:', response.data);
-      return response;
-    },
-    (error) => {
-      console.error('❌ 응답 실패:', error.response?.data || error.message);
-      return Promise.reject(error);
-    }
-  );
-console.log("api", api)
-export default api;
+  (config) => {
+    console.log('📤 요청 보냄:', config.url)
+    return config
+  },
+  (error) => {
+    console.error('🚨 요청 오류:', error)
+    return Promise.reject(error)
+  },
+)
+
+api.interceptors.response.use(
+  (response) => {
+    console.log('✅ 응답 성공:', response.data)
+    return response
+  },
+  (error) => {
+    console.error('❌ 응답 실패:', error.response?.data || error.message)
+    return Promise.reject(error)
+  },
+)
+console.log('api', api)
+export default api
