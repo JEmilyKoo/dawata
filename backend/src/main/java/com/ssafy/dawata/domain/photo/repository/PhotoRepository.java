811bdce78c7f02c81950aa7eb85c8c27e56c151f
이준호 <777joonho@kookmin.ac.kr>
2025-02-06 15:38:16 +0900

    feat(be): 약속 상세 보기 api에 참가자의 정보 추가

--- backend/src/main/java/com/ssafy/dawata/domain/photo/repository/PhotoRepository.java
+++ backend/src/main/java/com/ssafy/dawata/domain/photo/repository/PhotoRepository.java
@@ -1,5 +1,7 @@
 package com.ssafy.dawata.domain.photo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import com.ssafy.dawata.domain.photo.entity.Photo;
 @Repository
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
 	void deleteByPhotoName(String photoName);
+
+	Optional<Photo> findByEntityId(Long entityId);
 }
