40c4c35aa8f1fa31464e454f587c0e58be640531
최혁규 <gurrb19@naver.com>
2025-02-08 21:11:33 +0900

    refactor: 그룹 메인 페이지에서 캘린더를 제외한 Section들을 컴포넌트화 하여 분리

---
+++ frontend/src/app/club/hooks/useClubInfo.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'
+
+import api from '@/apis/api'
+import { Club } from '@/types/club'
+
+export const useClub = ({ clubId }: { clubId: number }) => {
+  const [clubInfo, setClubInfo] = useState<Club>()
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    const getClubInfoByClubId = async ({ clubId }: { clubId: number }) => {
+      try {
+        const response = await api.get(`/clubs/${clubId}`)
+        console.log('response.data', response.data)
+        setClubInfo(response.data)
+      } catch (error) {
+        console.error('⛔ 특정 그룹 데이터 조회 실패:', error)
+        return null
+      } finally {
+        setLoading(false)
+      }
+    }
+    getClubInfoByClubId({ clubId })
+  }, [])
+  return {
+    clubInfo,
+    loading,
+  }
+}
