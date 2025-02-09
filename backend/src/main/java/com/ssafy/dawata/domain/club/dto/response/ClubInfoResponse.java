c199a651ad7a1056ff34ae6fb13625f10a2201a7
김혜원 <cristine980@naver.com>
2025-02-09 14:27:58 +0900

    feat(be) : feed 기능 구현

--- backend/src/main/java/com/ssafy/dawata/domain/club/dto/response/ClubInfoResponse.java
+++ backend/src/main/java/com/ssafy/dawata/domain/club/dto/response/ClubInfoResponse.java
@@ -1,24 +1,26 @@
 package com.ssafy.dawata.domain.club.dto.response;
 
-import java.util.List;
-
 import com.ssafy.dawata.domain.club.entity.Club;
 import com.ssafy.dawata.domain.common.enums.Category;
+import java.util.List;
 
 public record ClubInfoResponse(
-	Long id,
-	String name,
-	Category category,
-	String teamCode,
-	List<ClubMemberInfoResponse> members
+    Long id,
+    String name,
+    Category category,
+    String teamCode,
+    String img,
+    List<ClubMemberInfoResponse> members
 ) {
-	public static ClubInfoResponse from(Club club, List<ClubMemberInfoResponse> members) {
-		return new ClubInfoResponse(
-			club.getId(),
-			club.getName(),
-			club.getCategory(),
-			club.getTeamCode(),
-			members
-		);
-	}
+
+    public static ClubInfoResponse from(Club club, List<ClubMemberInfoResponse> members) {
+        return new ClubInfoResponse(
+            club.getId(),
+            club.getName(),
+            club.getCategory(),
+            club.getTeamCode(),
+            club.getImg(),
+            members
+        );
+    }
 }
--- backend/src/main/java/com/ssafy/dawata/domain/club/entity/ClubMember.java
+++ backend/src/main/java/com/ssafy/dawata/domain/club/entity/ClubMember.java
@@ -1,8 +1,6 @@
 package com.ssafy.dawata.domain.club.entity;
 
-import com.ssafy.dawata.domain.common.entity.BaseTimeEntity;
 import com.ssafy.dawata.domain.member.entity.Member;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -16,78 +14,77 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "club_member")
-public class ClubMember extends BaseTimeEntity {
+public class ClubMember {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "member_id", nullable = false)
-	private Member member;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "club_id", nullable = false)
-	private Club club;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "club_id", nullable = false)
+    private Club club;
 
-	@Column(nullable = false)
-	private String nickname;
+    @Column(nullable = false)
+    private String nickname;
 
-	@Column(nullable = false)
-	private String clubName;
+    @Column(nullable = false)
+    private String clubName;
 
-	@Column(nullable = false, columnDefinition = "INT DEFAULT 1")
-	private int createdBy;
+    @Column(nullable = false, columnDefinition = "INT DEFAULT 1")
+    private int createdBy;
 
-	@Builder
-	public ClubMember(Member member, Club club, String nickname, String clubName, int createdBy) {
-		this.member = member;
-		this.club = club;
+    @Builder
+    public ClubMember(Member member, Club club, String nickname, String clubName, int createdBy) {
+        this.member = member;
+        this.club = club;
 
-		if (nickname != null) {
-			this.nickname = nickname;
-		} else {
-			this.nickname = member.getName();
-		}
+        if (nickname != null) {
+            this.nickname = nickname;
+        } else {
+            this.nickname = member.getName();
+        }
 
-		if (clubName != null) {
-			this.clubName = clubName;
-		} else {
-			this.clubName = club.getName();
-		}
+        if (clubName != null) {
+            this.clubName = clubName;
+        } else {
+            this.clubName = club.getName();
+        }
 
-		this.createdBy = createdBy;
-	}
+        this.createdBy = createdBy;
+    }
 
-	public static ClubMember createClubMember(Member member, Club club, int createdBy) {
-		return ClubMember.builder()
-			.member(member)
-			.club(club)
-			.nickname(member.getName())
-			.clubName(club.getName())
-			.createdBy(createdBy)
-			.build();
-	}
+    public static ClubMember createClubMember(Member member, Club club, int createdBy) {
+        return ClubMember.builder()
+            .member(member)
+            .club(club)
+            .nickname(member.getName())
+            .clubName(club.getName())
+            .createdBy(createdBy)
+            .build();
+    }
 
-	public void updateNickname(String nickname) {
-		this.nickname = nickname;
-	}
+    public void updateNickname(String nickname) {
+        this.nickname = nickname;
+    }
 
-	public void setNickname(String nickname) {
-		this.nickname = nickname;
-	}
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
 
-	public void setClubName(String clubName) {
-		this.nickname = nickname;
-	}
+    public void setClubName(String clubName) {
+        this.clubName = clubName;
+    }
 
-	public void setCreatedBy(int createdBy) {
-		this.createdBy = createdBy;
-	}
+    public void setCreatedBy(int createdBy) {
+        this.createdBy = createdBy;
+    }
 }
