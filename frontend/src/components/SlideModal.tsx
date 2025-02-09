32356ea71a3f912858b6798614f22301b925775d
JEmilyKoo <Jemilykoo@gmail.com>
2025-02-09 22:41:17 +0900

    feat: SlideModal UI 분리, autoClose 기능 추가

--- frontend/src/components/SlideModal.tsx
+++ frontend/src/components/SlideModal.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react"
-import { Text, TouchableOpacity, View } from "react-native"
-import Modal from "react-native-modal"
-
+import { View } from "react-native"
+import SlideModalUI from "@/components/SlideModalUI"
 interface SlideModalProps {
   modalButton: React.ReactElement // 부모로부터 전달받을 버튼
   modalTitle: string
@@ -10,6 +9,7 @@ interface SlideModalProps {
   primaryButtonOnPress: () => void
   secondaryButtonText: string
   secondaryButtonOnPress: () => void
+  autoClose? : boolean
 }
 
 const SlideModal: React.FC<SlideModalProps> = ({
@@ -20,10 +20,11 @@ const SlideModal: React.FC<SlideModalProps> = ({
   primaryButtonOnPress,
   secondaryButtonText,
   secondaryButtonOnPress,
+  autoClose = true,
 }: SlideModalProps) => {
   const [modalVisible, setModalVisible] = useState(false)
-
   const toggleModal = () => {
+    console.log("v toggleModal modalVisible", modalVisible)
     setModalVisible(!modalVisible)
   }
 
@@ -37,45 +38,34 @@ const SlideModal: React.FC<SlideModalProps> = ({
     },
   })
 
+  const handlePrimaryButtonPress = () => {
+    primaryButtonOnPress()
+    if (autoClose) {
+      setModalVisible(false)
+    }
+  }
+
+
+  const handleSecondaryButtonPress = () => {
+    secondaryButtonOnPress()
+    if (autoClose) {
+      setModalVisible(false)
+    }
+  }
+
   return (
     <View className="flex-1 justify-center items-center">
       {modalButtonWithToggle}
-
-      <Modal
+      <SlideModalUI
+        modalTitle={modalTitle}
+        modalContent={modalContent}
+        primaryButtonText={primaryButtonText}
+        primaryButtonOnPress={handlePrimaryButtonPress}
+        secondaryButtonText={secondaryButtonText}
+        secondaryButtonOnPress={handleSecondaryButtonPress}
         isVisible={modalVisible}
-        onBackdropPress={toggleModal}
-        swipeDirection="down"
-        onSwipeComplete={toggleModal}
-        animationIn="slideInUp"
-        animationOut="slideOutDown"
-        backdropColor="black"
-        backdropOpacity={0.5}
-        style={{ margin: 0, justifyContent: "flex-end" }}>
-        <View className="w-full p-5 bg-white rounded-lg items-center">
-          <Text className="w-80 text-center text-lg font-medium text-text-primary">
-            {modalTitle}
-          </Text>
-          <Text className="w-80 text-center text-sm font-regular text-text-secondary">
-            {modalContent}
-          </Text>
-
-          <TouchableOpacity
-            className="w-80 mt-4 px-4 py-4 bg-primary rounded-full"
-            onPress={primaryButtonOnPress}>
-            <Text className="text-white text-center font-bold">
-              {primaryButtonText}
-            </Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            className="w-80 mt-4 px-4 py-4 mb-4 border border-primary rounded-full"
-            onPress={secondaryButtonOnPress}>
-            <Text className="text-primary text-center text-primary font-bold">
-              {secondaryButtonText}
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </Modal>
+        setVisible={setModalVisible}
+      />
     </View>
   )
 }
