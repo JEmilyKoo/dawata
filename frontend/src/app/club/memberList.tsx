60fcb9e0166d5eda816e905c8d53e5625cef06dd
Huk9uri <gurrb19@naver.com>
2025-02-10 21:58:51 +0900

    feat: memberList.tsx에 hook 적용 확인 완료

--- frontend/src/app/club/memberList.tsx
+++ frontend/src/app/club/memberList.tsx
@@ -1,9 +1,37 @@
-import { SafeAreaView, Text } from 'react-native'
+import { Image, SafeAreaView, ScrollView, Text, View } from 'react-native'
 
-export default function ClubMemberList() {
+import { useLocalSearchParams, useRouter } from 'expo-router'
+
+import { useClubMemberList } from './hooks/useClubMemberList'
+
+type RouteParams = {
+  clubId: string
+}
+
+export const ClubMemberList = () => {
+  const params = useLocalSearchParams<RouteParams>()
+  const clubId = Number(params.clubId)
+  const { clubMemberList } = useClubMemberList({ clubId })
   return (
     <SafeAreaView>
-      <Text>클럽 멤버 목록</Text>
+      <ScrollView>
+        <Text style={{ fontSize: 20, fontWeight: 'bold', marginVertical: 10 }}>
+          클럽 멤버 목록
+        </Text>
+        {clubMemberList.map((item, index) => (
+          <View
+            key={index}
+            style={{ flexDirection: 'row', padding: 10, alignItems: 'center' }}>
+            <View>
+              <Text style={{ fontSize: 16, fontWeight: 'bold' }}>
+                {item.nickname}
+              </Text>
+              <Text>{item.email}</Text>
+            </View>
+          </View>
+        ))}
+        {clubMemberList.length === 0 && <Text>멤버가 없습니다.</Text>}
+      </ScrollView>
     </SafeAreaView>
   )
 }
