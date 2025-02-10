83c51f44ccca0d30399e666ac064ad2e18667aa9
Huk9uri <gurrb19@naver.com>
2025-02-10 18:10:03 +0900

    bugfix : api response 수정

--- frontend/src/app/club/create1.tsx
+++ frontend/src/app/club/create1.tsx
@@ -1,29 +1,32 @@
+import { useState } from 'react'
 import { Controller, useForm } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
-import { Text, TextInput, TouchableOpacity, View, SafeAreaView } from 'react-native'
+import {
+  SafeAreaView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
-import { useState } from 'react'
 
 import { Picker } from '@react-native-picker/picker'
 import { useRouter } from 'expo-router'
+import { NavigationOptions } from 'expo-router/build/global-state/routing'
 
+import { createClub } from '@/apis/club'
+import SlideModalUI from '@/components/SlideModalUI'
 import Category from '@/constants/Category'
 import { RootState } from '@/store/store'
 import { ClubCreateInfo } from '@/types/club'
 
-import {
-  setCreateCategory,
-  setCreateName,
-} from '../../store/slices/clubSlice'
-import { createClub } from '@/apis/club'
-import SlideModalUI from '@/components/SlideModalUI'
-import { NavigationOptions } from 'expo-router/build/global-state/routing'
+import { setCreateCategory, setCreateName } from '../../store/slices/clubSlice'
 
 const ClubCreate1 = () => {
   const { t } = useTranslation()
   const { create } = useSelector((state: RootState) => state.club)
   const [isVisible, setIsVisible] = useState(false)
-  const [clubId, setClubId] = useState(0);
+  const [clubId, setClubId] = useState(0)
   const {
     control,
     handleSubmit,
@@ -43,7 +46,7 @@ const ClubCreate1 = () => {
     const result = await createClub(data)
 
     if (result) {
-      setClubId(result.id)
+      setClubId(result.clubId)
       setIsVisible(true)
     }
   }
@@ -65,59 +68,59 @@ const ClubCreate1 = () => {
     <SafeAreaView className="flex-1 p-4 bg-white">
       <View className="flex-1 justify-between">
         <View>
-      <Text className="text-xl font-bold mb-2 text-text-primary">
-
-        {t('createClub.name.title')}
-      </Text>
-      <Text className="text-xs font-bold mb-2 text-text-secondary">
-        {t('createClub.name.subTitle')}
-      </Text>
-      <Controller
-        control={control}
-        name="name"
-        rules={{ required: t('createClub.name.error') }}
-        render={({ field: { onChange, onBlur, value } }) => (
-          <TextInput
-            placeholder={t('createClub.name.title')}
-            onBlur={onBlur}
-            className="border-b-2 mb-4 border-primary"
-            onChangeText={onChange}
-            value={value}
+          <Text className="text-xl font-bold mb-2 text-text-primary">
+            {t('createClub.name.title')}
+          </Text>
+          <Text className="text-xs font-bold mb-2 text-text-secondary">
+            {t('createClub.name.subTitle')}
+          </Text>
+          <Controller
+            control={control}
+            name="name"
+            rules={{ required: t('createClub.name.error') }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                placeholder={t('createClub.name.title')}
+                onBlur={onBlur}
+                className="border-b-2 mb-4 border-primary"
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
           />
-        )}
-      />
-      {errors.name && (
-        <Text className="text-light-red">{errors.name.message}</Text>
-      )}
+          {errors.name && (
+            <Text className="text-light-red">{errors.name.message}</Text>
+          )}
 
-      <Text className="text-xl font-bold mb-2">
-        {t('createClub.category.title')}
-      </Text>
-      <Controller
-        control={control}
-        name="category"
-        render={({ field }) => (
-          <Picker
-            selectedValue={field.value}
-            onValueChange={field.onChange}
-            className="border-2 p-2 mb-4">
-            {
-              Category.map((item) => (
-                <Picker.Item
-                  key={item}
-                  label={t(`category.${item}`)}
-                  value={item}
-                />
-              ))}
-          </Picker>
-        )}
-      />
-      </View>
-      <TouchableOpacity
-        className="bg-primary p-2 rounded"
-        onPress={handleSubmit(onSubmit)}>
-        <Text className="text-white text-center font-bold">{t('finish')}</Text>
-      </TouchableOpacity>
+          <Text className="text-xl font-bold mb-2">
+            {t('createClub.category.title')}
+          </Text>
+          <Controller
+            control={control}
+            name="category"
+            render={({ field }) => (
+              <Picker
+                selectedValue={field.value}
+                onValueChange={field.onChange}
+                className="border-2 p-2 mb-4">
+                {Category.map((item) => (
+                  <Picker.Item
+                    key={item}
+                    label={t(`category.${item}`)}
+                    value={item}
+                  />
+                ))}
+              </Picker>
+            )}
+          />
+        </View>
+        <TouchableOpacity
+          className="bg-primary p-2 rounded"
+          onPress={handleSubmit(onSubmit)}>
+          <Text className="text-white text-center font-bold">
+            {t('finish')}
+          </Text>
+        </TouchableOpacity>
       </View>
       <SlideModalUI
         isVisible={isVisible}
@@ -130,7 +133,6 @@ const ClubCreate1 = () => {
         secondaryButtonOnPress={onPressGoToHome}
       />
     </SafeAreaView>
-
   )
 }
 
