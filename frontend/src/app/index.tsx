6924b4d839214ba18c1c0b0387f4fdb67e10d648
bosung17 <ytv9802@gmail.com>
2025-02-14 18:21:16 +0900

    feat:카카오 로그인 테스트

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,8 +1,9 @@
 import { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Text, View } from 'react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
 
 import { getKeyHashAndroid } from '@react-native-kakao/core'
+import { login } from '@react-native-kakao/user'
 import { Link } from 'expo-router'
 
 export default function TabOneScreen() {
@@ -15,6 +16,12 @@ export default function TabOneScreen() {
       <Text>{t('index.title')}</Text>
       <Text>{t('index.catchphrase')}</Text>
       <Link href="/(tabs)/main">{t('index.lookArround')}</Link>
+      <TouchableOpacity
+        onPress={() => {
+          login()
+        }}>
+        <Text>기도메타</Text>
+      </TouchableOpacity>
     </View>
   )
 }
