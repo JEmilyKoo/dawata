50fe97fe21568c113822fccf0ff546d9f2386056
JEmilyKoo <Jemilykoo@gmail.com>
2025-02-17 05:11:27 +0900

    feat: package upgrade

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -2,20 +2,21 @@ module.exports = function (api) {
   api.cache(true)
   return {
     presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
+      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+      'nativewind/babel',
     ],
     plugins: [
-      "react-native-reanimated/plugin",
+      'react-native-reanimated/plugin',
       [
-        "babel-plugin-module-resolver",
+        'babel-plugin-module-resolver',
         {
-          root: ["./src"],
+          root: ['./src'],
           alias: {
-            app: "./src/app",
+            '@': './src',
+            app: './src/app',
           },
         },
-      ]
+      ],
     ],
   }
 }
