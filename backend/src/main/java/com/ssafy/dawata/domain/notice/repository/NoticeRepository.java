af4da62dad264e9f317d9daa42533907a216e61c
고성태 <v12rhtjdxo@naver.com>
2025-02-21 01:54:37 +0900

    feat(be) : 알림 요청 사항 적용
    - str로 문장 return

--- backend/src/main/java/com/ssafy/dawata/domain/notice/repository/NoticeRepository.java
+++ backend/src/main/java/com/ssafy/dawata/domain/notice/repository/NoticeRepository.java
@@ -1,6 +1,7 @@
 package com.ssafy.dawata.domain.notice.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -18,7 +19,7 @@ public interface NoticeRepository extends JpaRepository<Notice, Long> {
 	@Query("""
 		    SELECT n
 		    FROM Notice n
-		    JOIN Member m ON n.member = m
+		    JOIN FETCH Member m ON n.member = m
 		    WHERE n.member.id = :memberId
 		    AND n.deleted = false
 		""")
