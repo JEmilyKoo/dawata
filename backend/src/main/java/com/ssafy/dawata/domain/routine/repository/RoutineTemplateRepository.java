46f57ed3d183ca7b0d5e0c66a44d389a3dcdb589
주노리 <777joonho@kookmin.ac.kr>
2025-02-21 02:36:02 +0900

    fix(be): unsafe 쿼리문 수정

--- backend/src/main/java/com/ssafy/dawata/domain/routine/repository/RoutineTemplateRepository.java
+++ backend/src/main/java/com/ssafy/dawata/domain/routine/repository/RoutineTemplateRepository.java
@@ -4,6 +4,7 @@ import com.ssafy.dawata.domain.member.entity.Member;
 import com.ssafy.dawata.domain.routine.dto.response.RoutineElementResponse;
 import com.ssafy.dawata.domain.routine.dto.response.RoutineTemplateResponse;
 import com.ssafy.dawata.domain.routine.entity.RoutineTemplate;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -16,42 +17,42 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RoutineTemplateRepository extends JpaRepository<RoutineTemplate, Long> {
 
-    @Query("""
-            SELECT new com.ssafy.dawata.domain.routine.dto.response.RoutineTemplateResponse(
-                rt.id,
-                rt.name,
-                COALESCE(SUM(re.spendTime), 0)
-            )
-            FROM RoutineTemplate rt
-            JOIN RoutineElement re ON rt.id = re.routineTemplate.id
-            WHERE rt.member.id = :memberId
-            GROUP BY rt.id, rt.name
-        """)
-    Slice<RoutineTemplateResponse> customFindAllByMemberId(@Param("memberId") Long id);
+	@Query("""
+		    SELECT new com.ssafy.dawata.domain.routine.dto.response.RoutineTemplateResponse(
+		        rt.id,
+		        rt.name,
+		        IFNULL(SUM(re.spendTime), 0L)
+		    )
+		    FROM RoutineTemplate rt
+		    JOIN RoutineElement re ON rt.id = re.routineTemplate.id
+		    WHERE rt.member.id = :memberId
+		    GROUP BY rt.id, rt.name
+		""")
+	Slice<RoutineTemplateResponse> customFindAllByMemberId(@Param("memberId") Long id);
 
-    @Query("""
-          SELECT new com.ssafy.dawata.domain.routine.dto.response.RoutineElementResponse (
-        	  re.id,
-        	  re.play,
-        	  re.spendTime
-          )
-          FROM RoutineElement re
-          WHERE re.routineTemplate.id = :routineId
-          ORDER BY re.sequence
-        """)
-    List<RoutineElementResponse> customFindByRoutineId(@Param("routineId") Long routineId);
+	@Query("""
+		  SELECT new com.ssafy.dawata.domain.routine.dto.response.RoutineElementResponse (
+			  re.id,
+			  re.play,
+			  re.spendTime
+		  )
+		  FROM RoutineElement re
+		  WHERE re.routineTemplate.id = :routineId
+		  ORDER BY re.sequence
+		""")
+	List<RoutineElementResponse> customFindByRoutineId(@Param("routineId") Long routineId);
 
-    RoutineTemplate save(RoutineTemplate routineTemplate);
+	RoutineTemplate save(RoutineTemplate routineTemplate);
 
-    List<RoutineTemplate> findAllByMember(Member member);
+	List<RoutineTemplate> findAllByMember(Member member);
 
-    @Query("""
-          SELECT rt
-          FROM RoutineTemplate  rt
-          WHERE rt.id = :routineId AND rt.member.id = :memberId
-        """)
-    Optional<RoutineTemplate> findByIdAndMemberId(
-        @Param("routineId")Long routineId,
-        @Param("memberId")Long memberId
-    );
+	@Query("""
+		  SELECT rt
+		  FROM RoutineTemplate  rt
+		  WHERE rt.id = :routineId AND rt.member.id = :memberId
+		""")
+	Optional<RoutineTemplate> findByIdAndMemberId(
+		@Param("routineId") Long routineId,
+		@Param("memberId") Long memberId
+	);
 }
